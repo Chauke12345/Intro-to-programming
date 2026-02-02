d707b1f9d3f2db3f4b9e5ca070bfcd7bb2a622f0
Chauke12345 <edwardchauke@hotmail.com>
2026-02-02 15:56:44 +0200

    Add files via upload

---
+++ BOOK2/Chapter 2/escape.js
@@ -0,0 +1,7 @@
+// Example 1:
+let text = "we are so-called \"Vikings\" from the north.";
+console.log(text); // Output: we are so-called "Vikings" from the north.
+
+// Example 2:
+let text2 = 'It\'s a beautiful day!';
+console.log(text2); // Output: It's a beautiful day!
